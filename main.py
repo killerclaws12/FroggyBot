6237169d660983daf0587ceadc7fdbd2a6c82dfa
Killerclaws12 <hiltonbrennan70@gmail.com>
2023-05-27 13:53:52 -0400

    setup cogs

--- main.py
+++ main.py
@@ -14,8 +14,11 @@ async def on_ready() -> None:
 
     print("Bot is now online.")
 ###########################################################################################
+for filename in os.listdir("./cogs"):
+    if filename.endswith(".py") and filename != "__init__.py":
+        bot.load_extension(f"cogs.{filename[:-3]}")
+###########################################################################################
 load_dotenv()
 
 bot.run(os.getenv("DISCORD_TOKEN"))
-
 ###########################################################################################
